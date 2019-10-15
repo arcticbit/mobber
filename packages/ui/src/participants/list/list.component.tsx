e4f0de4cc41f898a03c4abb9737aeb5c8240cb23
Martin Persson <martin.l.persson@axis.com>
2019-10-15 08:42:09 +0200

    Extract participant form

--- packages/ui/src/participants/list/list.component.tsx
+++ packages/ui/src/participants/list/list.component.tsx
@@ -1,47 +1,11 @@
-import React from "react";
-import "./list.component.scss";
-import { IPerson } from "../../../../model/person.model";
-import { Api } from "../../app/api";
-import { Participant } from "../participant/participant.component";
-import { FlagIcon } from "../../flag-icon/flag-icon.component";
+import './list.component.scss';
 
+import React from 'react';
 
-const styles = {
-  formInput: {
-    flex: "auto",
-    boxSizing: "border-box" as any,
-    padding: "8px",
-    outline: 'none',
-    border: 0,
-  },
-  formSubmitButton: {
-    flex: "1",
-    maxWidth: "30px",
-    background: "#eee"
-  },
-  formFlags: {
-    padding: "4px",
-    cursor: 'pointer'
-  },
-  flag: {
-    width: '20px',
-    padding: '0 5px'
-  },
-  form: {
-    border: '1px solid #eee',
-    background: '#fff',
-    margin: "0 10px",
-    flex: "auto",
-    minHeight: "30px",
-    maxHeight: "30px",
-    display: "flex"
-  }
-};
+import { Participant } from '../participant/participant.component';
+import { AddParticipant } from '../add-participant/add-participant';
 
 export class Participants extends React.Component<any, any> {
-  private api = new Api();
-  public state: Partial<{ name: string, layout: string}> = { name: '', layout: 'Swedish-Pro' };
-
   render() {
     return (
       this.props &&
@@ -54,70 +18,9 @@ export class Participants extends React.Component<any, any> {
               onDriverPromotion={this.props.onDriverPromotion}
             />
           ))}
-          <div style={styles.form}>
-            <div style={styles.formFlags} onClick={this.toggleLayout}>
-
-              {
-                this.state.layout == 'US'
-                  ? <FlagIcon country={'US' as any} size={ 20 }/>
-                  : <FlagIcon country={'Swedish-Pro' as any} size={ 20 } />
-              }
-              {/* <img onClick={() => this.setState({layout: 'US'})} src={usFlag} style={{ ...this.getFlagStyle('US'), ...styles.flag}} />
-              <img onClick={() => this.setState({layout: 'Swedish-Pro'})} src={seFlag} style={{ ...this.getFlagStyle('Swedish-Pro'), ...styles.flag}} /> */}
-            </div>
-            <input
-              id="inputField"
-              style={styles.formInput}
-              type="text"
-              placeholder="New participant"
-              onKeyPress={this.handleFormKeydown}
-              onChange={this.handleFormChange}
-              value={this.state.name}
-            />
-          </div>
+          <AddParticipant />
         </div>
       )
     );
   }
-
-  private toggleLayout = () => {
-    this.setState({
-      layout: this.state.layout === 'US'
-        ? 'Swedish-Pro'
-        : 'US'
-    });
-  }
-
-  private getFlagStyle = (countryCode: string) => {
-    return { filter: this.state.layout !== countryCode ? 'saturate(0%)' : '' }
-  }
-
-  private handleFormFlag = (e: any) => {
-    console.log(e);
-  }
-
-  private handleFormKeydown = (e: any) => {
-    if (e.key.toLowerCase() !== "enter") {
-      return;
-    }
-    this.handleFormSubmit();
-  };
-
-  private handleFormSubmit = () => {
-    const newParticipant: IPerson = {
-      name: this.state.name || '',
-      language: "Swedish-Pro" as any,
-      scroll: "Natural" as any
-    };
-    this.setState({ name: "" });
-    this.api.add(newParticipant);
-  };
-
-  private handleFormChange = (e: any) => {
-    this.setState({
-      ...this.state,
-        name: e.target.value,
-        layout: this.state.layout
-      });
-  };
 }
